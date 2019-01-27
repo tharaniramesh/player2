2ba9dd048aeb49c55a7155fe826d8cd821d48d3b
tharaniramesh <46441125+tharaniramesh@users.noreply.github.com>
2019-01-27 04:13:38 -0800

    Update sorted_array.py

--- sorted_array.py
+++ sorted_array.py
@@ -2,7 +2,7 @@ a,b=map(str,input().split())
 c=list(map(str,input().split()))
 for i in c:
       if i in b:
-            print('yes')
+            print('Yes')
             break
 else:
-      print('no')
+      print('No')
