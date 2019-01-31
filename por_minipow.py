f06a0bd9fd5af1a1bdce92e8aba6faeb1e3f72a4
tharaniramesh <46441125+tharaniramesh@users.noreply.github.com>
2019-01-31 16:00:57 +0530

    Create por_minipow.py

---
+++ por_minipow.py
@@ -0,0 +1,19 @@
+#thara
+def pro(n):
+    for i in range(1,n+1):
+        if 2**i==n:
+            return True
+def pow1(n):
+    a=0
+    for j in range(1,n+1):
+        if pro(n):
+            a==j
+    return a        
+
+n=int(input())
+if pro(n):
+    print('0')
+    
+else:
+    a=pow1(n)
+    print('1')
